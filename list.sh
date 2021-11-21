7d2ea0c31f054be530e31db9e61526f006abfabb
Ahmed Khalaf <59212055+Ahmedkhalaf477@users.noreply.github.com>
2021-11-21 03:49:35 +0200

    Add files via upload

---
+++ list.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo " Welcome To In The My Worled."
+echo "  By Ahmed_Khalaf " 
+echo ================================
+pkg istall crunch
+ echo ================================
+echo " Don...!  Follow In Facebook .."
+echo " https://www.facebook.com/Ahmedkhkh477 "
+echo " By Ahmed_Khalaf "
+echo  " congratyoultions... done..! "
+echo ================================
